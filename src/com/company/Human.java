491814137375dde78822991a98fc06b9436b0898
Dawid Reclaf <dawid.reclaf@enterosoft.com>
2021-10-29 21:25:18 +0200

    [Zadanie_1]: code prep

---
+++ src/com/company/Human.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Human {
+    public String firstNAme;
+    public String lastNAme;
+    public Animal pet;
+    private Double salary;
+    protected String phone;
+}
