806448343c45a2e0b0495ebb541729a944fc8410
Dawid Reclaf <dawid.reclaf@enterosoft.com>
2021-10-29 23:18:43 +0200

    [Zadanie_3_4]: excersise solution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,24 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Car audi = new Car("A6", "Audi", "Granatowy", 2.4, "Wuuuuuuu", 10000.0);
         Human person = new Human("Dawid", "Reclaf");
 
         person.getSalary();
         System.out.println("\n");
-        person.buyCar(audi);
 
         System.out.println("\n");
 
         person.setSalary(1000.0);
         System.out.println("\n");
-        person.buyCar(audi);
+        person.getSalary();
 
         System.out.println("\n");
 
         person.setSalary(11000.0);
         System.out.println("\n");
-        person.buyCar(audi);
+        person.getSalary();
 
         System.out.println("\n");
 
