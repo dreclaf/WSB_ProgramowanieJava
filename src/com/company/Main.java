3a5a69fecf8b7106b7737ee522de7bc588342c44
Dawid Reclaf <dawid.reclaf@enterosoft.com>
2022-01-08 17:34:46 +0100

    [Zadanie_7]: Code deployment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,32 +7,11 @@ import com.company.devices.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Pet pies = new Pet("dog", "Majki");
+        Phone telefon = new Phone("2001", "Galaxy", "Samsuns", 6.3, "Android");
+        Car auto = new Car("A6", "Audi", "2004", "Czarny", 2.4, "Wuuuuuu", 20000.00);
 
-        Human Human1 = new Human("Dawid", "Reclaf", "123 123 123");
-        Human Human2 = new Human("Krzysztof", "Wicka", "321 321 321");
-        Human Human3 = new Human("Marek", "Markowski", "555 555 555");
-
-
-        Human1.setCash(1200.00);
-
-        try{
-            pies.sale(Human2, Human1, 1000.00);
-        }
-        catch(Exception e){
-            System.out.println(e.getMessage());
-        }
-        
-
-        Human2.pet = pies;
-
-        pies.sale(Human2, Human1, 1000.00);
-
-        System.out.println(Human1.toString());
-        System.out.println(Human2.toString());
-
-
-        Human3.sale(Human2, Human1, 10.00);
+        telefon.turnOn();
+        auto.turnOn();
 
 
     }
