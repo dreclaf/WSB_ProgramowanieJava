d807f2e7295b07149e0f8a74f42b87e4afcf002e
Dawid Reclaf <dawid.reclaf@enterosoft.com>
2022-01-08 17:31:07 +0100

    [Zadanie_8]: code deployment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,34 @@ import com.company.devices.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        FarmAnimal swinia = new FarmAnimal("swinia");
+    public static void main(String[] args) throws Exception {
         Pet pies = new Pet("dog", "Majki");
 
-        swinia.feed(300);
-        swinia.beEaten();
-        pies.petThePet();
+        Human Human1 = new Human("Dawid", "Reclaf", "123 123 123");
+        Human Human2 = new Human("Krzysztof", "Wicka", "321 321 321");
+        Human Human3 = new Human("Marek", "Markowski", "555 555 555");
+
+
+        Human1.setCash(1200.00);
+
+        try{
+            pies.sale(Human2, Human1, 1000.00);
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+
+        Human2.pet = pies;
+
+        pies.sale(Human2, Human1, 1000.00);
+
+        System.out.println(Human1.toString());
+        System.out.println(Human2.toString());
+
+
+        Human3.sale(Human2, Human1, 10.00);
+
 
     }
 }
